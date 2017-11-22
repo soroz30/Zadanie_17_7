6f170260d3975f601fbb7e3f0ecc6d4f7b0981eb
soroz30 <soroz30@yahoo.pl>
2017-11-22 15:57:20 +0100

    Zadanie 17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '272837429676-meutlbbpa8edkchv27u7c3uhd7obs80k.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'c2EVP3ByZDRAROfEQSqqKLoj',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
